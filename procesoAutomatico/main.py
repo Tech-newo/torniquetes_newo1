81226d1235812232ba0c0a724d4f5044ba79c1b2
Santiago Arango <sntg98@hotmail.com>
2021-10-12 08:53:11 -0500

    proceso automatico_ discretizacion de elementos interactuables por id de elemento

--- procesoAutomatico/main.py
+++ procesoAutomatico/main.py
@@ -16,39 +16,55 @@ driver.get('http://localhost:8100/')
 
 # TODO 3: validate the login status
 def is_login():
-  if driver.current_url == 'http://localhost:8100/':
-    return False
-  else:
-    return True
+    if driver.current_url == 'http://localhost:8100/':
+        return False
+    else:
+        return True
 
 
 # TODO 2: login admin user
 def login_administrator():
-  user = driver.find_element_by_xpath("//*[contains(@aria-labelledby,'ion-input-0-lbl')]")
-  user.click()
-  user.clear()
-  user.send_keys("admin")
-  password = driver.find_element_by_xpath("//*[contains(@aria-labelledby,'ion-input-1-lbl')]")
-  password.click()
-  password.clear()
-  password.send_keys("Gpsglobal2014")
-  location_code = driver.find_element_by_xpath("//*[contains(@aria-labelledby,'ion-input-2-lbl')]")
-  location_code.click()
-  location_code.clear()
-  location_code.send_keys("1502,0")
-  button_login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]')))
-  button_login.click()
-  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="outputMessage"]')))
+    user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@aria-labelledby,'ion-input-0-lbl')]")))
+    user.click()
+    user.clear()
+    user.send_keys("admin")
+    password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@aria-labelledby,'ion-input-1-lbl')]")))
+    password.click()
+    password.clear()
+    password.send_keys("Gpsglobal2014")
+    location_code = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@aria-labelledby,'ion-input-2-lbl')]")))
+    location_code.click()
+    location_code.clear()
+    location_code.send_keys("1502,0")
+    button_login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]')))
+    button_login.click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="outputMessage"]')))
 
 
 def process_data():
-  message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="outputMessage"]')))
-  print(message.text)
-  is_internet_on = False
+    message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="outputMessage"]')))
+    print(message.text)
+    if(message.text == 'registro Exitoso'):
+        print('Registro Exitoso')
+        '''
+        elif (message.text == 'sin_internet'):
+          print('sin_internet')
+        '''
+    elif(message.text == 'Escanea tu QR en el lector'):
+        input_qr_code = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="qrCodeInput"]/input')))
+        input_qr_code.click()
+        input_qr_code.clear()
+        input_qr_code.send_keys("2,121061,1502")
+        input_qr_code.send_keys(Keys.ENTER)
+
+    elif (message.text =='no es podible registrar la salida, debido a que el ultimo registro es una salida'):
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="outputMessage"]')))
+    else:
+        is_internet_on = False
 
 
 while is_internet_on:
-  if is_login():
-    process_data()
-  else:
-    login_administrator()
+    if is_login():
+        process_data()
+    else:
+        login_administrator()
